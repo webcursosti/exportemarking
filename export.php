81f4d2f338514f98438d8d8f2e9edbfb937b10e5
webcursosti <webcursosti@webcursoss-imac.uai.cl>
2018-11-06 17:43:42 -0300

    ecuaciones diferenciales

--- export.php
+++ export.php
@@ -36,7 +36,7 @@ global $DB, $CFG;
 // Validate that we have a rubric associated
 $query = 'SELECT e.*, cm.id as coursemoduleid from mdl_course as c
 inner join mdl_emarking as e ON c.id = e.course
-inner join mdl_course_modules as cm on e.id = cm.instance AND cm.module = 36 where c.fullname LIKE "%cálculo%"  AND c.shortname like "%MAT%" AND c.shortname like "%2-2018" AND e.name LIKE "%Prueba 2%" order by c.fullname';
+inner join mdl_course_modules as cm on e.id = cm.instance AND cm.module = 36 where c.fullname LIKE "%Ecuaciones Diferenciales%"  AND c.shortname like "%MAT%" AND c.shortname like "%2-2018" AND e.name LIKE "%Prueba 2%" order by c.fullname';
 
 $emarkings = $DB->get_records_sql($query);
 $return = array();
